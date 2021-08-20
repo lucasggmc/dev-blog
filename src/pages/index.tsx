2473fd7c932ff53129d0f070b0dfd179824fb6a8
lucasggmc <lucasggmc@gmail.com>
2021-08-19 22:11:29 -0300

    create home page layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
 
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { FiCalendar, FiUser, FiClock } from 'react-icons/fi';
 
 interface Post {
   uid?: string;
@@ -24,9 +26,60 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  // TODO
+
+  return(
+    <>
+      <Head>
+        <title>Home | dev-blog</title>
+      </Head>
+      <main className={styles.container}>               
+          <header className={styles.homeHeader}>
+          <img src="/icons/greater-less-sign.svg" alt="" />
+          <p>spacetraveling</p>          
+          </header>
+
+            <div className={styles.postContainer}>
+              <h1>Como utilizar Hooks</h1>
+              <p>Pensando em sincronização em vez de ciclos de vida.</p>
+              <footer>
+                <div>
+                  <span>
+                    <FiCalendar />
+                  15 Mar 2021
+                  </span>
+                  <span>
+                    <FiUser />
+                    Joseph Oliveira
+                  </span>
+                </div>
+              </footer>
+            </div>      
+
+            <div className={styles.postContainer}>
+              <h1>Como utilizar Hooks</h1>
+              <p>Pensando em sincronização em vez de ciclos de vida.</p>
+              <footer>
+                <div>
+                  <span>
+                    <FiCalendar />
+                  15 Mar 2021
+                  </span>
+                  <span>
+                    <FiUser />
+                    Joseph Oliveira
+                  </span>
+                </div>
+              </footer>
+            </div>   
+
+           <span className={styles.loadPosts}>Carregar mais posts</span>
+
+      </main>
+    </>
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
